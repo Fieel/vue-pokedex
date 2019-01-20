22898ad26efa4603fdfc607aee939cc23e435f71
Filipe Madureira <contact@filipemadureira.com>
2019-01-20 18:13:51 +0100

    Updated README, edited the default view pages.

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # vue-pokedex
 
+This is a web-based Pokédex built for educational purposes, in fact this project will help me pick up and learn the Vue.js framework quickly.
+
+## PokéAPI
+
+I'll be using the PokéAPI to build this web-based Pokédex: https://pokeapi.co/
+
+## Links
+
+* Vue.js docs: https://vuejs.org/v2/guide/
+* VueCLI docs: https://cli.vuejs.org/guide/
+* Vuex docs: https://vuex.vuejs.org/guide/
+* PokéAPI docs: https://pokeapi.co/docs/
+
+## Tests
+
+I'll try and write tests along the way, by default i'll use the tools provided by the VueCLI by default:
+
+* e2e: Cypress https://docs.cypress.io/ 
+* Assertions: Chai https://www.chaijs.com/guide/
+* Runner: Mocha https://mochajs.org/
+
 ## Project setup
 ```
 npm install
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <PokemonCard />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { Component, Vue } from 'vue-property-decorator';
+  import PokemonCard from '@/components/PokemonCard.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      PokemonCard,
+    },
+  })
+  export default class Home extends Vue {}
 </script>
